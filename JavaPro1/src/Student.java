7125f35b3afcf2fb8686487da0c0f60e3279417e
abc <abc@gmail.com>
2021-11-24 09:55:22 +0530

    Student class added

---
+++ JavaPro1/src/Student.java
@@ -0,0 +1,19 @@
+
+public class Student {
+	private int roll;
+	private String sname;
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+
+}
